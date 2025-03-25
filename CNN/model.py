fada58e82a69b9f6549f1a1b0075687c5a7f9cfa
liuximeng2 <sliu484@emory.edu>
2025-03-24 21:44:16 -0400

    updated training of CNN

--- CNN/model.py
+++ CNN/model.py
@@ -2,27 +2,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class GraphConvolution(nn.Module):
-    def __init__(self, pixel):
+    def __init__(self, pixel, in_channels=3, hidden_channels=(16, 32, 64), fc_sizes=(128, 1), 
+                 pool_stride=2, reduction_factor=8, kernel_size=3, stride=1, padding=1):
         super(GraphConvolution, self).__init__()
         self.pixel = pixel
-        self.convoluted_pixel = pixel / 8
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.reduction_factor = reduction_factor
+        self.convoluted_pixel = pixel // self.reduction_factor
+        
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels[0], 
+                              kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv2 = nn.Conv2d(in_channels=hidden_channels[0], out_channels=hidden_channels[1], 
+                              kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv3 = nn.Conv2d(in_channels=hidden_channels[1], out_channels=hidden_channels[2], 
+                              kernel_size=kernel_size, stride=stride, padding=padding)
 
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=kernel_size, stride=pool_stride)
 
-        self.fc1 = nn.Linear(64 * 81 * 81, 128)
-        self.fc2 = nn.Linear(128, 2)
+        # Calculate conv_dim based on pooling operations
+        out = self.pixel
+        for _ in range(3):
+            out = (out - kernel_size) // pool_stride + 1
+        self.conv_dim = hidden_channels[2] * out * out
+
+        self.fc1 = nn.Linear(self.conv_dim, fc_sizes[0])
+        self.fc2 = nn.Linear(fc_sizes[0], fc_sizes[1])
+
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
-        x = x.view(-1, 64 * 81 * 81)
-
+        x = x.view(-1, self.conv_dim)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        x = self.sigmoid(x)
 
         return x
--- CNN/run.py
+++ CNN/run.py
@@ -3,77 +3,99 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Training on {device}')
 
-pixel = 648
-num_epoch = 5
+pixel = 64
+num_epoch = 20
 
-variable = transforms.Compose(
-    [
-        transforms.Resize((pixel,pixel)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(
-        [0.5 for _ in range(3)], [0.2 for _ in range(3)]),
-    ]
-)
-train_data = datasets.ImageFolder(root = 'data/chest_xray/train/', transform =variable)
-val_data = datasets.ImageFolder(root = 'data/chest_xray/val/', transform =variable)
-test_data = datasets.ImageFolder(root = 'data/chest_xray/test/', transform =variable)
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=8, shuffle=True)
-validloader = torch.utils.data.DataLoader(val_data, batch_size=8, shuffle=True)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=8, shuffle=True)
+# read data from .npy file: Data/Data Transformed/X_train.npy
+x_train = np.load('Data/Data Transformed/X_train.npy')
+y_train = np.load('Data/Data Transformed/y_train.npy')
+x_test = np.load('Data/Data Transformed/X_test.npy')
+y_test = np.load('Data/Data Transformed/y_test.npy')
+# load the data into the DataLoader
+train_data = TensorDataset(torch.Tensor(x_train).permute(0, 3, 1, 2), torch.LongTensor(y_train))
+test_data = TensorDataset(torch.Tensor(x_test).permute(0, 3, 1, 2), torch.LongTensor(y_test))
+trainloader = DataLoader(dataset=train_data, batch_size=4, shuffle=True)
+testloader = DataLoader(dataset=test_data, batch_size=4, shuffle=False)
 
-model = GraphConvolution(pixel)
+print('Data loaded, Trainning set size is', len(train_data), 'Test set size is', len(test_data))
+
+model = GraphConvolution(pixel, kernel_size = 3)
 model.to(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+# Lists to store metrics history
+train_losses = []
+train_f1_scores = []
+test_f1_scores = []
 
 for epoch in range(num_epoch):
+    model.train()
     running_loss = 0.0
+    
     for i, data in enumerate(trainloader, 0):
-
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
+        labels = labels.float()
 
         optimizer.zero_grad()
-        if(inputs.size(1)) == 1:
-            inputs = torch.squeeze(inputs, 1)
-        outputs = model(inputs)
+        outputs = model(inputs).squeeze()
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
 
         running_loss += loss.item()
+    
+    # Calculate epoch metrics
+    model.eval()
+    epoch_loss = 0.0
+    all_train_preds = []
+    all_train_labels = []
+    all_test_preds = []
+    all_test_labels = []
+    
+    with torch.no_grad():
+        for data in trainloader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device).float()
+            if(inputs.size(1)) == 1:
+                inputs = torch.squeeze(inputs, 1)
+            outputs = model(inputs).squeeze()
+            loss = criterion(outputs, labels)
+            epoch_loss += loss.item()
+            predicted = torch.round(outputs)
+            
+            # Collect predictions and labels for F1 calculation
+            all_train_preds.extend(predicted.cpu().numpy())
+            all_train_labels.extend(labels.cpu().numpy())
+            
+        for data in testloader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device).float()
+            outputs = model(inputs).squeeze()
+            predicted = torch.round(outputs)
+            
+            # Collect predictions and labels for F1 calculation
+            all_test_preds.extend(predicted.cpu().numpy())
+            all_test_labels.extend(labels.cpu().numpy())
+    
+    train_loss = epoch_loss / len(trainloader)
+    train_f1 = f1_score(all_train_labels, all_train_preds, average='weighted')
+    test_f1 = f1_score(all_test_labels, all_test_preds, average='weighted')
+    
+    train_losses.append(train_loss)
+    train_f1_scores.append(train_f1)
+    test_f1_scores.append(test_f1)
+    
+    print(f'Epoch {epoch+1}/{num_epoch} - Train Loss: {train_loss:.4f}, Train F1: {train_f1:.4f}, Test F1: {test_f1:.4f}')
 
-        if i % 50 == 49:
-            print(f'[{epoch + 1}, {i + 1:5d}] train_loss: {running_loss / 50:.3f}')
-            running_loss = 0.0
-
-PATH = 'model/trained_cnn_model.pth'
-torch.save(model.state_dict(), PATH)
 print('Finished Training')
-
-model.to('cpu')
-correct = 0
-total = 0
-
-with torch.no_grad():
-    for data in testloader:
-        inputs, labels = data
-        # calculate outputs by running images through the network
-        outputs = model(inputs)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print(f'Accuracy of the CNN on the test images: {100 * correct // total} %')
